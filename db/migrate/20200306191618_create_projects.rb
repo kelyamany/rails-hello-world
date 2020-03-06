47bd89e115b8eeed2f13f1b483309c2f6a75e43b
Khaled <khaled.alyameny@gmail.com>
2020-03-06 20:24:48 +0100

    project migration syntax error

--- db/migrate/20200306191618_create_projects.rb
+++ db/migrate/20200306191618_create_projects.rb
@@ -1,11 +1,11 @@
 class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
-      t.string :[name
+      t.string :name
       t.integer :number
       t.string :city
       t.datetime :start
-      t.datetime] :end
+      t.datetime :end
 
       t.timestamps
     end
