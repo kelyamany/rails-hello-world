08a5ba067b7f21d8d35ceca74835eaf48de53762
Khaled <khaled.alyameny@gmail.com>
2020-03-06 20:16:38 +0100

    added project model

---
+++ db/migrate/20200306191618_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.string :[name
+      t.integer :number
+      t.string :city
+      t.datetime :start
+      t.datetime] :end
+
+      t.timestamps
+    end
+  end
+end
