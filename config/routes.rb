10fc9a22d1c1a0dd70c8fb79e9d2fb11b767ebd9
Khaled <khaled.alyameny@gmail.com>
2020-03-06 20:23:39 +0100

    added project controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  get 'projects/show'
+  get 'projects/new'
+  get 'projects/create'
+  get 'projects/edit'
+  get 'projects/update'
+  get 'projects/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
