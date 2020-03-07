af1b45e6d22c42b7379664fa1d24303e364b22a8
Khaled <khaled.alyameny@gmail.com>
2020-03-07 10:24:42 +0100

    route to main react component

---
+++ app/assets/javascripts/_main.js.jsx
@@ -0,0 +1,7 @@
+const Main = (props) => {
+    return(
+        <div>
+            <h1>Test Project Main</h1>
+        </div>
+    )
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root to: 'home#index'
   get 'projects/index'
   get 'projects/show'
   get 'projects/new'
