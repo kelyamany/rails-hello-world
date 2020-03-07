323eb325e2866cda0fc17bd1889206f39d3bce70
Khaled <khaled.alyameny@gmail.com>
2020-03-07 10:05:24 +0100

    project controller implementation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    render json: {
-        projects: [
-            {
-                name: "test",
-                number: 3
-            }
-        ]
-    }.to_json
+    render json: Project.all
   end
 
   def show
@@ -17,14 +10,26 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    project = Project.create(project_args)
+    render json: project
   end
 
   def edit
   end
 
   def update
+    project = Project.find(params[:id])
+    project.update_attributes(project_args)
+    render json: project
   end
 
   def destroy
+    Project.destroy(params[:id])
+  end
+
+  private
+
+  def project_args
+    params.require(:project).permit(:id, :name, :number, :city, :start, :end)
   end
 end
