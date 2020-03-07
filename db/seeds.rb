94d68cacf876e66ae98b46dcbaa54adf466d924b
Khaled <khaled.alyameny@gmail.com>
2020-03-07 10:14:09 +0100

    project seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+projects = ['project1', 'project2', 'project3']
+i = 0
+projects.each{|project| Project.create(name: project, number: i+=1 , city: "Vienna", start: DateTime.now, end: DateTime.new(2020, 0o3, 0o7, 17, 0o0,0o0))}
