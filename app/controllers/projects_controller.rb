71ed086ebbc51ba81f780e8151cdb0086210c81b
Khaled <khaled.alyameny@gmail.com>
2020-03-07 13:54:10 +0100

    consume the weather api (unlinked)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,21 @@
+# frozen_string_literal: true
+require 'uri'
+require 'net/http'
+require 'json'
+
 class ProjectsController < ApplicationController
   def index
     render json: Project.all
+  end
+
+  # TODO: Inject the weather result into the returned model objects
+  def get_weather(city: string)
+    url = URI("api.openweathermap.org/data/2.5/weather?q=#{city}&appid=ee91aa6de8a7b91fb9d1e8031d8aefa9")
+    http = Net::HTTP.new(url.host, url.port)
+    request = Net::HTTP::Get.new(url)
+    response = http.request(request)
+    json =  JSON.parse(response)
+    json['main']['temp']
   end
 
   def show
